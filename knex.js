ebdbb240f256e0cfea86b51570919c19f29cff6d
Nick <nbjenvey@gmail.com>
2019-06-18 21:40:05 +0000

    Knex search script

---
+++ knex.js
@@ -0,0 +1,24 @@
+const settings = require("./settings");
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+const name = process.argv.slice(2)[0];
+knex.from('famous_people').select("*").where('first_name', '=', name)
+  .then((people) => {
+    console.log("Searching...");
+    console.log(`Found ${people.length} person(s) by the name '${name}':`);
+    for (person of people) {
+      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${person.birthdate.toLocaleDateString()}'`);
+    }
+  })
+  .finally(() => {
+    knex.destroy();
+  });
